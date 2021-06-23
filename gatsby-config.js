33faabeffcd6975539c0ab0006346d1d1a47a515
Coocochen <827230613@qq.com>
2021-06-23 16:53:59 +0800

    feat: 初始化主题

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Kuzure Blog`,
+    author: `kuzure`,
+    description: `Kuzure's blog`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Themed } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      kuzure
+      <br />
+      东边有座山
+    </>
+  )
+}
